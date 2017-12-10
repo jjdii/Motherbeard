14152f9bf7ee638f73b17d6d0924409f4ef2b5a6
JJ Driscoll <james@jjdriscoll.com>
2017-12-10 00:29:42 -0500

    added leftIcons to drawer menuItems

--- app/src/components/drawer.js
+++ app/src/components/drawer.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import Drawer from 'material-ui/Drawer'
 import MenuItem from 'material-ui/MenuItem'
+import HomeIcon from 'material-ui/svg-icons/action/home'
+import BuildsIcon from 'material-ui/svg-icons/action/build'
+import CreateBuildIcon from 'material-ui/svg-icons/content/create'
+import SearchIcon from 'material-ui/svg-icons/action/search'
+import CartIcon from 'material-ui/svg-icons/action/shopping-cart'
 import RaisedButton from 'material-ui/RaisedButton'
 import history from '../history'
 import { Link } from 'react-router-dom'
@@ -33,21 +38,28 @@ export default class DrawerButton extends React.Component {
         </a>
         <Drawer
           docked={false}
-          width={200}
+          width={240}
           open={this.state.open}
           onRequestChange={open => this.setState({ open })}
+          containerStyle={{ background: '#ffffff' }}
         >
           <div
             style={{
               width: '100%',
               height: '105px',
-              borderBottom: '1px solid #d1d1d1'
+              borderBottom: '1px solid #d1d1d1',
+              background: '#ffffff'
             }}
           />
           <MenuItem
             onClick={() => {
               this.handleClose()
               history.push('/')
+            }}
+            leftIcon={<HomeIcon />}
+            style={{
+              background: '#ececec',
+              borderBottom: '1px solid #d1d1d1'
             }}
           >
             Home
@@ -57,6 +69,11 @@ export default class DrawerButton extends React.Component {
               this.handleClose()
               history.push('/builds')
             }}
+            leftIcon={<BuildsIcon />}
+            style={{
+              background: '#ececec',
+              borderBottom: '1px solid #d1d1d1'
+            }}
           >
             Builds
           </MenuItem>
@@ -64,6 +81,11 @@ export default class DrawerButton extends React.Component {
             onClick={() => {
               this.handleClose()
               history.push('/templates/new')
+            }}
+            leftIcon={<CreateBuildIcon />}
+            style={{
+              background: '#ececec',
+              borderBottom: '1px solid #d1d1d1'
             }}
           >
             Create Build
@@ -73,6 +95,11 @@ export default class DrawerButton extends React.Component {
               this.handleClose()
               history.push('/search')
             }}
+            leftIcon={<SearchIcon />}
+            style={{
+              background: '#ececec',
+              borderBottom: '1px solid #d1d1d1'
+            }}
           >
             Search
           </MenuItem>
@@ -80,6 +107,11 @@ export default class DrawerButton extends React.Component {
             onClick={() => {
               this.handleClose()
               history.push('/cart')
+            }}
+            leftIcon={<CartIcon />}
+            style={{
+              background: '#ececec',
+              borderBottom: '1px solid #d1d1d1'
             }}
           >
             Cart&nbsp;
